fb675a878a2c282fd0a7a7eca66c06688fcdf699
Daniel Boros <dancixx@gmail.com>
2024-05-27 12:46:57 +0200

    feat: add first element to subscription

--- src/graphql/subscriptions/users.rs
+++ src/graphql/subscriptions/users.rs
@@ -1,11 +1,16 @@
-use std::{sync::Arc, time::Duration};
-
 use async_graphql::{Context, Subscription};
-use surrealdb::{engine::remote::ws::Client, Surreal};
-use tokio::sync::RwLock;
-use tokio_stream::{Stream, StreamExt};
+use async_stream::stream;
+use surrealdb::{engine::remote::ws::Client, Notification, Result, Surreal};
+use tokio_stream::Stream;
 
 use crate::{graphql::types::surreal_id::SurrealID, models::users::User};
+
+fn handle(result: Result<Notification<User>>) -> Option<User> {
+    match result {
+        Ok(notification) => Some(notification.data),
+        Err(_) => None,
+    }
+}
 
 #[derive(Default)]
 pub struct UsersSubscriptionRoot;
@@ -15,25 +20,34 @@ impl UsersSubscriptionRoot {
     #[graphql(name = "subscribe_users_by_id")]
     pub async fn subscribe_users_by_id<'a>(
         &self,
-        context: &Context<'_>,
+        context: &'a Context<'_>,
         id: String,
     ) -> impl Stream<Item = Option<User>> + 'a {
         let surreal = context.data::<Surreal<Client>>().unwrap();
-        let surreal = Arc::new(RwLock::new(surreal.clone()));
         let SurrealID(thing) = SurrealID::from(id);
+        let initial_data = surreal
+            .select::<Option<User>>(("users", thing.id.clone()))
+            .await
+            .unwrap();
 
-        tokio_stream::wrappers::IntervalStream::new(tokio::time::interval(Duration::from_secs(1)))
-            .then(move |_| {
-                let surreal = surreal.clone();
-                let id = thing.id.clone();
+        let stream = surreal
+            .select::<Option<User>>(("users", thing.id.clone()))
+            .live()
+            .await
+            .unwrap();
 
-                async move {
-                    let surreal = surreal.read().await;
-                    match surreal.select::<Option<User>>(("users", id.clone())).await {
-                        Ok(Some(user)) => Some(user),
-                        _ => None,
-                    }
-                }
-            })
+        stream! {
+            yield initial_data;
+
+            // Works both ways but here needs to be mut stream
+            // while let Some(result) = stream.next().await {
+            //     yield handle(result);
+            // }
+
+            for await result in stream {
+                yield handle(result);
+            }
+
+        }
     }
 }
